e83d782e4294afc9d94c6891220dc6595cabf4f2
Justin Hileman <justin@justinhileman.info>
2012-09-09 04:39:26 -0700

    updates for v2.0 release

--- lib/default.rb
+++ lib/default.rb
@@ -1,2 +1,7 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
+
+def item_slug(item=nil)
+  item ||= @item
+  'item-' + item.identifier.gsub(/[^\w]+/, ' ').strip.gsub(' ', '-')
+end
