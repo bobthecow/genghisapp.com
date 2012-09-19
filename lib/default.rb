ccceb32ea6f30c7de0ef8606a67c5447eacae246
Justin Hileman <justin@justinhileman.info>
2012-09-19 15:22:30 -0700

    Fetch current Genghis version on compile.

--- lib/default.rb
+++ lib/default.rb
@@ -9,3 +9,9 @@ end
 def screenshots
   @items.select {|i| i.identifier =~ /^\/screenshots\/./ && i[:extension] == 'png' }
 end
+
+def get_genghis_version
+  require 'open-uri'
+  index = @items.find {|i| i.identifier == '/' }
+  index[:genghis_version] = open('https://raw.github.com/bobthecow/genghis/master/VERSION').read
+end
